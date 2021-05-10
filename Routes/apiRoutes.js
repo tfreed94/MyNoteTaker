cec278e49512cda20cfa5be6994026be7444eeef
Troy Freed <tfreed94@gmail.com>
2021-05-10 18:28:10 -0400

    added API routes to js file

---
+++ Routes/apiRoutes.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+const dbJSON = require('./db/db.json');
+ 
+module.exports = (app) => {
+    let savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    app.get("/api/notes", (req, res) => {
+        return res.json(savedNotes);
+    });
+    app.post('/api/notes', (req, res) => {
+        let finalID;
+        if (savedNotes.length) {
+            finalID = Math.max(...(savedNotes.map(note => note.id)));
+        } else {
+            finalID = 0;
+        }
+        const noteID = finalID + 1;
+        savedNotes.push({ noteID, ...req.body });
+        res.json(savedNotes.slice(-1));
+    });
+};
