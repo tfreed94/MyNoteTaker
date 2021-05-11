fcb7df574c3abfb01acc7ac2bf79b104af989f91
Troy Freed <tfreed94@gmail.com>
2021-05-10 20:56:23 -0400

    added code to server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 4500;
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+app.use(express.static("public"));
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
